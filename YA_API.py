3d991d3397dd72e907161bc1e49f96d41aa7b417
SRS <rs.sku@yandex.ru>
2024-02-20 23:55:16 +0300

    Work done

--- YA_API.py
+++ YA_API.py
@@ -12,7 +12,8 @@ id_ = input('Введите Ваш ID или screen_name без пробелов
 count_ = input('Введите количество копируемых фото (по умолчанию 5)')
 if count_ == "":
     count_ = 5
-count_ = int(count_)
+else:
+    count_ = int(count_)
 
 
 class YaApi:
